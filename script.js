6215c1041b9d1983a046143bf6c775fccd30183b
zakwan dhiya <zakwandhiya@gmail.com>
2025-06-23 00:34:48 +0700

    update game mechanism and readme

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-// const cards = document.querySelectorAll('.card');
-
-// let selectedCardIndex = -1;
-
-// cards.forEach((card, index) => {
-//     card.addEventListener('click', () => {
-//         if (selectedCardIndex !== -1) {
-//             cards.forEach(c => c.classList.remove('selected'));
-//         }
-//         selectedCardIndex = index;
-//         card.classList.add('selected');
-//     });
-// });
-
 let score = 0;
 let numbersOnDeck = [];
+
+document.getElementById('userInput').addEventListener('input', function(e) {
+    let inputValue = e.target.value;
+    let numbers = extractNumbers(inputValue);
+    let highlightIndex=[false,false,false,false]
+    for (let i = 0; i < numbers.length; i++) {
+        for (let k = 0; k < 4; k++) {
+            if(numbers[i] == numbersOnDeck[k]){
+                highlightIndex[k] = true;
+            }
+        }
+    }
+
+    for (let i = 0; i < 4; i++) {
+        if(highlightIndex[i]){
+            document.getElementById(`card${i+1}`).classList.add('selected');
+        }else{
+            document.getElementById(`card${i+1}`).classList.remove('selected');
+        }
+    }
+    
+});
+
+function search(ele) {
+    if(event.key === 'Enter') {
+        evaluateExpression();       
+    }
+}
+
 
 function evaluateExpression() {
     const userInput = document.getElementById('userInput').value;
@@ -24,7 +39,7 @@ function evaluateExpression() {
         console.log("semua angka yang diberikan harus digunakan satu dan hanya satu kali")
         score -= 50;
         document.getElementById('scoreDisplay').innerText = `Score: ${score}`;
-        document.getElementById('errorDisplay').innerText = `semua angka yang diberikan harus digunakan satu dan hanya satu kali`;
+        document.getElementById('errorDisplay').innerText = `semua angka yang diberikan harus \ndigunakan satu dan hanya satu kali`;
         return;
     }
     for (let i = 0; i < 4; i++) {
@@ -38,7 +53,7 @@ function evaluateExpression() {
         console.log("semua angka yang diberikan harus digunakan satu dan hanya satu kali")
         score -= 50;
         document.getElementById('scoreDisplay').innerText = `Score: ${score}`;
-        document.getElementById('errorDisplay').innerText = `semua angka yang diberikan harus digunakan satu dan hanya satu kali`;
+        document.getElementById('errorDisplay').innerText = `semua angka yang diberikan harus \ndigunakan satu dan hanya satu kali`;
         return;
     }
     calculate(userInput);
@@ -63,14 +78,17 @@ function calculate(userInput) {
             document.getElementById('scoreDisplay').innerText = `Score: ${score}`;
             
             document.getElementById('errorDisplay').innerText = ``;
+            for (let i = 0; i < 4; i++) {
+                document.getElementById(`card${i+1}`).classList.remove('selected');
+            }
+            document.getElementById('userInput').value = '';
             refreshNumber();
         } else {
             console.log(`hasil dari persamaan tersebut adalah ${result}`)
             score -= 50;
             document.getElementById('scoreDisplay').innerText = `Score: ${score}`;
-            document.getElementById('errorDisplay').innerText = `hasil dari persamaan tersebut adalah ${result}`;
+            document.getElementById('errorDisplay').innerText = `hasil dari persamaan tersebut \nadalah ${result}`;
         }
-        document.getElementById('userInput').value = '';
     } catch (e) {
         alert('Invalid expression. Please try again.');
         document.getElementById('scoreDisplay').innerText = `Score: ${score}`;
@@ -81,6 +99,10 @@ function skipThisDeck() {
     score -= 200;
     document.getElementById('scoreDisplay').innerText = `Score: ${score}`;
     document.getElementById('errorDisplay').innerText = ``;
+    document.getElementById('userInput').value = '';
+    for (let i = 0; i < 4; i++) {
+        document.getElementById(`card${i+1}`).classList.remove('selected');
+    }
     refreshNumber();
 }
 
